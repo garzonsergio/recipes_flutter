7551d0ddb90f6e573889deb7d94ac153100186af
scgarzonp <scgarzonp@gmail.com>
2024-11-27 11:28:06 -0500

    Feature: The Scaffold was included in the app

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'First commit',
-      home: Container(
-        child: Text('First commit'),
-      ),
+      home: RecipeBook(),
     );
+  }
+}
+
+class RecipeBook extends StatelessWidget {
+  const RecipeBook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text(
+          "Recipe Book",
+          style: TextStyle(color: Colors.black54),
+        )),
+        body: Container(child: Text('Welcome to Recipe Book')));
   }
 }
