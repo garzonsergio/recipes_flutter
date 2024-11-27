4bd1944bc74746026175b37b29b929a4f8c84f87
scgarzonp <scgarzonp@gmail.com>
2024-11-27 11:48:52 -0500

    Feature: A card with different containers also was set a media query to config the width

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'First commit',
       home: RecipeBook(),
@@ -22,10 +22,31 @@ class RecipeBook extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            title: Text(
-          "Recipe Book",
-          style: TextStyle(color: Colors.black54),
-        )),
-        body: Container(child: Text('Welcome to Recipe Book')));
+            backgroundColor: Colors.red[900],
+            title: const Text(
+              "Recipe Book",
+              style: TextStyle(color: Colors.black54),
+            )),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height:125,
+          child: Card(
+              child: Row(children: <Widget>[
+            Container(
+                height: 125,
+                width: 100,
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Container())),
+            const SizedBox(
+              width: 20,
+            ),
+            Column(children: <Widget>[
+              Text("AAA"),
+              Text("BBB"),
+              Container(height: 2, width: 125, color: Colors.amber[700])
+            ])
+          ])),
+        ));
   }
 }
