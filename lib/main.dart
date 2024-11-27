a1b88782efede75fd468e0f74720dc47ef7d5b32
scgarzonp <scgarzonp@gmail.com>
2024-11-27 11:53:34 -0500

    Fix:Comments included to understand better the code

--- lib/main.dart
+++ lib/main.dart
@@ -28,20 +28,20 @@ class RecipeBook extends StatelessWidget {
               style: TextStyle(color: Colors.black54),
             )),
         body: Container(
-          width: MediaQuery.of(context).size.width,
+          width: MediaQuery.of(context).size.width, //Media query to set the width of the container to the width of the screen
           height:125,
           child: Card(
               child: Row(children: <Widget>[
             Container(
                 height: 125,
                 width: 100,
-                child: ClipRRect(
+                child: ClipRRect( //This widget is a Rectangle with rounded corners
                     borderRadius: BorderRadius.circular(10),
                     child: Container())),
             const SizedBox(
               width: 20,
             ),
-            Column(children: <Widget>[
+            Column(children: <Widget>[// Is possible to use a column to organize the widgets vertically
               Text("AAA"),
               Text("BBB"),
               Container(height: 2, width: 125, color: Colors.amber[700])
