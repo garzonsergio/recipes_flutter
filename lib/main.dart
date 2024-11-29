806fe20cf4554954c5f4f7dd41e4d1e0acc3f32b
scgarzonp <scgarzonp@gmail.com>
2024-11-29 09:33:14 -0500

    Feature:There was implemented a controller to handle the tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipes_flutter/screens/home_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -15,38 +16,33 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class RecipeBook extends StatelessWidget {
+class RecipeBook extends StatelessWidget{
   const RecipeBook({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
         appBar: AppBar(
-            backgroundColor: Colors.red[900],
-            title: const Text(
-              "Recipe Book",
-              style: TextStyle(color: Colors.black54),
-            )),
-        body: Container(
-          width: MediaQuery.of(context).size.width, //Media query to set the width of the container to the width of the screen
-          height:125,
-          child: Card(
-              child: Row(children: <Widget>[
-            Container(
-                height: 125,
-                width: 100,
-                child: ClipRRect( //This widget is a Rectangle with rounded corners
-                    borderRadius: BorderRadius.circular(10),
-                    child: Container())),
-            const SizedBox(
-              width: 20,
-            ),
-            Column(children: <Widget>[// Is possible to use a column to organize the widgets vertically
-              Text("AAA"),
-              Text("BBB"),
-              Container(height: 2, width: 125, color: Colors.amber[700])
-            ])
-          ])),
-        ));
+          backgroundColor: Colors.red[900],
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home), text: 'Home'),
+              Tab(icon: Icon(Icons.search), text: 'Search')
+            ],
+          ),
+          title: const Text(
+            "Recipe Book",
+            style: TextStyle(color: Colors.black54),
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            HomeScreen(),
+          ],
+        ),
+      ),
+    );
   }
 }
