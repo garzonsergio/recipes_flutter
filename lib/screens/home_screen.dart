9d0916b163a12650cb1339f5663ca8656e4258b8
scgarzonp <scgarzonp@gmail.com>
2024-12-02 11:05:59 -0500

    Feature:There was created a form inside the modal

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,13 +6,34 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red[900],
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            _showBottom(context);
+          },
+        ),
         body: Column(
-      children: <Widget>[
-        _RecipesCard(context),
-        _RecipesCard(context),
-        _RecipesCard(context),
-      ],
-    ));
+          children: <Widget>[
+            _RecipesCard(context),
+            _RecipesCard(context),
+            _RecipesCard(context),
+          ],
+        ));
+  }
+
+  Future<void> _showBottom(BuildContext context) {
+    return showModalBottomSheet(
+        context: context,
+        builder: (context) => Container(
+              width: MediaQuery.of(context).size.width,
+              height: 500,
+              color: Colors.amber,
+              child: const RecipeForm(),
+            ));
   }
 
   Widget _RecipesCard(BuildContext context) {
@@ -26,12 +47,14 @@ class HomeScreen extends StatelessWidget {
         child: Card(
             child: Row(children: <Widget>[
           Container(
-              height: 125,
-              width: 100,
-              child: ClipRRect(
-                  //This widget is a Rectangle with rounded corners
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container())),
+            height: 125,
+            width: 100,
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.network(
+                    'https://cloudfront-us-east-1.images.arcpublishing.com/elespectador/JLYGWDUSXFDI7ITQECOXNAG674.jpg',
+                    fit: BoxFit.cover)),
+          ),
           const SizedBox(
             width: 20,
           ),
@@ -40,12 +63,43 @@ class HomeScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 // Is possible to use a column to organize the widgets vertically
-                Text("AAA"),
-                Text("BBB"),
-                Container(height: 2, width: 125, color: Colors.amber[700])
+                Text("Recipe name",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Quicksand')),
+                Container(height: 2, width: 125, color: Colors.amber[700]),
+                Text(
+                  "Author",
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+                SizedBox(height: 4),
               ])
         ])),
       ),
     );
+  }
+}
+
+class RecipeForm extends StatelessWidget {
+  const RecipeForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Form(
+            // key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Add a new recipe',
+                    style: TextStyle(
+                        color: Colors.deepOrangeAccent[500], fontSize: 24)),
+                TextFormField()
+              ],
+            )));
   }
 }
