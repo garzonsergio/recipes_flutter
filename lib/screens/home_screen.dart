5018e09aeea9fc7d3f74e86c48fd01382ad312ab
scgarzonp <scgarzonp@gmail.com>
2024-12-03 15:08:32 -0500

    Feature:There was created other fields in the form

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +31,7 @@ class HomeScreen extends StatelessWidget {
         builder: (context) => Container(
               width: MediaQuery.of(context).size.width,
               height: 500,
-              color: Colors.amber,
+              color: Colors.white,
               child: const RecipeForm(),
             ));
   }
@@ -39,14 +39,14 @@ class HomeScreen extends StatelessWidget {
   Widget _RecipesCard(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
+      child: SizedBox(
         width: MediaQuery.of(context)
             .size
             .width, //Media query to set the width of the container to the width of the screen
         height: 125,
         child: Card(
             child: Row(children: <Widget>[
-          Container(
+          SizedBox(
             height: 125,
             width: 100,
             child: ClipRRect(
@@ -63,19 +63,19 @@ class HomeScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 // Is possible to use a column to organize the widgets vertically
-                Text("Recipe name",
+                const Text("Recipe name",
                     style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
                         fontFamily: 'Quicksand')),
                 Container(height: 2, width: 125, color: Colors.amber[700]),
-                Text(
+                const Text(
                   "Author",
                   style: TextStyle(
                     fontSize: 12,
                   ),
                 ),
-                SizedBox(height: 4),
+                const SizedBox(height: 4),
               ])
         ])),
       ),
@@ -89,17 +89,47 @@ class RecipeForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(8.0),
         child: Form(
-            // key: _formKey,
+            key: _formKey,
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Add a new recipe',
-                    style: TextStyle(
-                        color: Colors.deepOrangeAccent[500], fontSize: 24)),
-                TextFormField()
-              ],
-            )));
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('Add a new recipe',
+                style: TextStyle(color: Colors.black, fontSize: 24)),
+            const SizedBox(
+              height: 16,
+            ),
+            _buildTextField(label: "Name of the recipe"),
+            const SizedBox(
+              height: 16,
+            ),
+            _buildTextField(label: "Author"),
+            const SizedBox(
+              height: 16,
+            ),
+            _buildTextField(label: 'Image Url'),
+            const SizedBox(
+              height: 16,
+            ),
+            _buildTextField(label: 'Recipe'),
+            const SizedBox(
+              height: 16,
+            ),
+          ],
+        )));
   }
+}
+
+Widget _buildTextField({required String label}) {
+  return TextFormField(
+    decoration: InputDecoration(
+      labelText: label,
+      labelStyle: const TextStyle(fontFamily: 'Quicksand', color: Colors.black),
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+      focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: Colors.black, width: 1)),
+    ),
+  );
 }
