56a4b5964c0c39b59c2fc7c5574edad392f51693
scgarzonp <scgarzonp@gmail.com>
2024-12-03 15:45:06 -0500

    Fix:There was created the validators and additional properties to TextFormFields

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -30,7 +30,7 @@ class HomeScreen extends StatelessWidget {
         context: context,
         builder: (context) => Container(
               width: MediaQuery.of(context).size.width,
-              height: 500,
+              height: 600,
               color: Colors.white,
               child: const RecipeForm(),
             ));
@@ -88,40 +88,101 @@ class RecipeForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+
+    final TextEditingController _recipeName = TextEditingController();
+    final TextEditingController _recipeAuthor = TextEditingController();
+    final TextEditingController _recipeImage = TextEditingController();
+    final TextEditingController _recipeDescription = TextEditingController();
+
     return Padding(
         padding: const EdgeInsets.all(8.0),
         child: Form(
             key: _formKey,
             child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text('Add a new recipe',
-                style: TextStyle(color: Colors.black, fontSize: 24)),
-            const SizedBox(
-              height: 16,
-            ),
-            _buildTextField(label: "Name of the recipe"),
-            const SizedBox(
-              height: 16,
-            ),
-            _buildTextField(label: "Author"),
-            const SizedBox(
-              height: 16,
-            ),
-            _buildTextField(label: 'Image Url'),
-            const SizedBox(
-              height: 16,
-            ),
-            _buildTextField(label: 'Recipe'),
-            const SizedBox(
-              height: 16,
-            ),
-          ],
-        )));
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Add a new recipe',
+                    style: TextStyle(color: Colors.black, fontSize: 24)),
+                const SizedBox(
+                  height: 16,
+                ),
+                _buildTextField(
+                    controller: _recipeName,
+                    label: "Name of the recipe",
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the name of the recipe';
+                      }
+                    }),
+                const SizedBox(
+                  height: 16,
+                ),
+                _buildTextField(
+                    controller: _recipeAuthor,
+                    label: "Author",
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the name of the author';
+                      }
+                    }),
+                const SizedBox(
+                  height: 16,
+                ),
+                _buildTextField(
+                    controller: _recipeImage,
+                    label: 'Image Url',
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                    }),
+                const SizedBox(
+                  height: 16,
+                ),
+                _buildTextField(
+                    maxLines: 4,
+                    controller: _recipeDescription,
+                    label: 'Recipe',
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                    }),
+                const SizedBox(
+                  height: 16,
+                ),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        Navigator.pop(context);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orangeAccent,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      // padding: const EdgeInsets.symmetric(
+                      //     horizontal: 32, vertical: 16)
+                    ),
+                    child: const Text(
+                      'Save Recipe',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                )
+              ],
+            )));
   }
 }
 
-Widget _buildTextField({required String label}) {
+Widget _buildTextField(
+    {required String label,
+    required TextEditingController controller,
+    required String? Function(String?) validator,
+    int maxLines = 1}) {
   return TextFormField(
     decoration: InputDecoration(
       labelText: label,
@@ -131,5 +192,7 @@ Widget _buildTextField({required String label}) {
           borderRadius: BorderRadius.circular(10),
           borderSide: BorderSide(color: Colors.black, width: 1)),
     ),
+    validator: validator,
+    maxLines: maxLines,
   );
 }
