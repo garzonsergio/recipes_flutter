ace1290f3e62c10af3f037e454bc17acc698e536
scgarzonp <scgarzonp@gmail.com>
2024-12-09 09:47:31 -0500

    Feature:There was created a model of recipe

---
+++ lib/models/recipe_model.dart
@@ -0,0 +1,34 @@
+class Recipe {
+  String name;
+  String author;
+  String image_link;
+  List<String> recipeSteps;
+
+  Recipe(
+      {required this.name,
+      required this.author,
+      required this.image_link,
+      required this.recipeSteps});
+
+  factory Recipe.fromJSON(Map<String, dynamic> json) {
+    return Recipe(
+        name: json['name'],
+        author: json['author'],
+        image_link: json['image_link'],
+        recipeSteps: List<String>.from(json['recipe']));
+  }
+
+  Map<String, dynamic> toJSON() {
+    return {
+      'name': name,
+      'author': author,
+      'image_link': image_link,
+      'recipe': recipeSteps
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Recipe{name: $name, author: $author, image_link: $image_link, recipeSteps: $recipeSteps}';
+  } 
+}
