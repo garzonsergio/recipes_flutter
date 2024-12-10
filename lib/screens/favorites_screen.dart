89ac3b4335c86507711bdce14b778024c8561c5b
scgarzonp <scgarzonp@gmail.com>
2024-12-10 08:45:28 -0500

    Fix:There was create a favoriteCard and the favorite screen now is showing the list of favorites

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,20 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:recipes_flutter/models/recipe_model.dart';
+import 'package:recipes_flutter/providers/recipes_provider.dart';
+import 'package:recipes_flutter/screens/recipe_detail.dart';
 
-
-class FavoritesScreen extends StatefulWidget{
+class FavoritesScreen extends StatefulWidget {
   const FavoritesScreen({super.key});
-  
-  @override 
+
+  @override
   State<FavoritesScreen> createState() => _FavoritesScreenState();
 }
 
-class _FavoritesScreenState extends State<FavoritesScreen>{
+class _FavoritesScreenState extends State<FavoritesScreen> {
   @override
-  Widget build(BuildContext context){
-    return const Center(
-      child: Text('Favorites Screen'),
-    );
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Consumer<RecipesProvider>(
+            builder: (context, recipeProvider, child) {
+          final favoritesRecipes = recipeProvider.favoriteRecipe;
 
+          return favoritesRecipes.isEmpty
+              ? Center(child: Text('No favorites found'))
+              : ListView.builder(
+                  itemCount: favoritesRecipes.length,
+                  itemBuilder: (context, index) {
+                    return favoriteRecipesCard(recipe: favoritesRecipes[index]);
+                  },
+                );
+        }));
   }
 }
 
+class favoriteRecipesCard extends StatelessWidget {
+  final Recipe recipe;
+  const favoriteRecipesCard({super.key, required this.recipe});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => RecipeDetail(recipesData: recipe)));
+        },
+        child: Card(
+            child: Column(
+          children: [
+            // Image.network(recipe.imageLink),
+            Text(recipe.name),
+            Text(recipe.author)
+          ],
+        )));
+  }
+}
--- lib/screens/recipe_detail.dart
+++ lib/screens/recipe_detail.dart
@@ -49,24 +49,3 @@ class _RecipeDetailState extends State<RecipeDetail> {
   }
 }
 
-// class RecipeDetail extends StatelessWidget {
-//   final String recipeName;
-//   const RecipeDetail({super.key, required this.recipeName});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(recipeName, style: TextStyle(color: Colors.white)),
-//         backgroundColor: Colors.red[900],
-//         leading: IconButton(
-//           icon: const Icon(Icons.arrow_back),
-//           color: Colors.white,
-//           onPressed: () {
-//             Navigator.pop(context);
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
